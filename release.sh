b1297abb3019ac3788beef03de5a94e72f46cdc3
Sebastian Eckstein <ecki@fem.tu-ilmenau.de>
2011-12-27 04:21:14 +0000

    a
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@629 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- release.sh
+++ release.sh
@@ -4,10 +4,11 @@ mv /mnt/data/release/"$1" /mnt/data/release/torrent/.
 cd /mnt/data/release/torrent/
 sha1sum -b "$1" >> "$1".sha1
 
-mktorrent -a http://v4.torrent.speedpartner.de:6969/announce -a http://v6.torrent.speedpartner.de:6969/announce -a http://tracker.publicbt.com:80/announce,udp://tracker.publicbt.com:80/announce -a http://tracker.openbittorrent.com:80/announce,udp://tracker.openbittorrent.com:80/announce -a http://tracker.birkenwald.de:6969/announce -a http://tracker.torrent.to:2710/announce -a http://exodus.1337x.org/announce -w http://mirror.fem-net.de/CCC/28C3/"$2"/"$1" -l 19 "$1" -o "$1".torrent
+mktorrent -a http://v6.torrent.speedpartner.de:6969/announce -a http://v4.torrent.speedpartner.de:6969/announce -a http://tracker.publicbt.com:80/announce,udp://tracker.publicbt.com:80/announce -a http://tracker.openbittorrent.com:80/announce,udp://tracker.openbittorrent.com:80/announce -a http://tracker.birkenwald.de:6969/announce -a http://tracker.torrent.to:2710/announce -a http://exodus.1337x.org/announce -w http://mirror.fem-net.de/CCC/28C3/"$2"/"$1" -l 19 "$1" -o "$1".torrent
 
 ln /mnt/data/release/torrent/"$1".sha1 /mnt/data/mirror/28C3/"$2"/"$1".sha1
 ln /mnt/data/release/torrent/"$1".torrent /mnt/data/mirror/28C3/"$2"/"$1".torrent
+echo "# $1" >> /mnt/data/release/tracker/28c3.txt
 transmission-show "$1".torrent | awk '/Hash/ {print $2}' >> /mnt/data/release/tracker/28c3.txt
 lftp -f /dev/stdin <<EOF
 set cmd:fail-exit true;
