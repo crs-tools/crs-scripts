18058db678413bcffd0ef31f195d1b7089e4832f
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2013-12-28 19:12:51 +0000

    weitere anpassungen
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@980 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- scripts/script-D-encoding-postprocessor.pl
+++ scripts/script-D-encoding-postprocessor.pl
@@ -6,17 +6,16 @@ require boolean;
 
 # Call this script with secret and project slug as parameter!
 
-my ($secret, $project, $token) = ($ENV{'CRS_SECRET'}, $ENV{'CRS_SLUG'}, $ENV{'CRS_TOKEN'});
+my ($secret, $token) = ($ENV{'CRS_SECRET'}, $ENV{'CRS_TOKEN'});
 
-if (!defined($project)) {
+if (!defined($token)) {
 	# print usage
 	print STDERR "Too few parameters given!\nUsage:\n\n";
-	print STDERR "./script-.... <secret> <project slug>\n\n";
+	print STDERR "./script-.... <secret> <token>\n\n";
 	exit 1;
 }
 
 my $tracker = C3TT::Client->new('https://tracker.fem.tu-ilmenau.de/rpc', $token, $secret);
-#$tracker->setCurrentProject($project);
 my $ticket = $tracker->assignNextUnassignedForState('encoding', 'postencoding');
 
 if (!defined($ticket) || ref($ticket) eq 'boolean' || $ticket->{id} <= 0) {
@@ -40,23 +39,22 @@ if (!defined($ticket) || ref($ticket) eq 'boolean' || $ticket->{id} <= 0) {
 		$tracker->setTicketFailed($tid, 'Encoding postprocessor: srcfile '.$srcfile.' not found!');
 	}
 	my $destbasename = $props->{'Processing.Path.Release'} . '/' . $props->{'Fahrplan.ID'};
-	my $destfile = $destbasename . "." . $props->{'EncodingProfile.Extension'};
-#	my $return = system ("scp -i /root/.ssh/id_rsa $srcfile ecki\@chief-mirror.fem-net.de:~/release/ ");
-
-	my $return = system ("cp $srcfile $destfile");
+	my $destfile = $destbasename . "-" . $props->{'EncodingProfile.Slug'} . '.' . $props->{'EncodingProfile.Extension'};
+	my $return = system ("scp -i /root/.ssh/id_rsa $srcfile fem\@10.73.0.11:/opt/crs/encoded/ ");
+	#my $return = system ("cp $srcfile $destfile");
 	print "$srcfile (in) -> $destfile (out) - exit=$return\n";
 	
 	# generate thumbnails
 	my $offset = $props->{'Record.DurationSeconds'}/2;
 	print "generating thumbnails at $offset seconds\n";
-	my $return2 = system("ffmpeg -ss $offset -i $srcfile -y -f image2 -vframes 1 -s 768x432 $destbasename.jpg");
-	$return2 = system("ffmpeg -ss $offset -i $srcfile -y -f image2 -vframes 1 -s 334x188 $destbasename-thumb.jpg");
+#	my $return2 = system("ffmpeg -ss $offset -i $srcfile -y -f image2 -vframes 1 -s 768x432 $destbasename.jpg");
+#	$return2 = system("ffmpeg -ss $offset -i $srcfile -y -f image2 -vframes 1 -s 334x188 $destbasename-thumb.jpg");
 
 	# write metadata back to tracker (?)
 
 	if ($return eq '0') {
-		$tracker->setTicketDone($tid, 'Encoding postprocessor: copy to chief-mirror completed.');
+		$tracker->setTicketDone($tid, 'Encoding postprocessor: scp to brisky completed.');
 	} else {
-		$tracker->setTicketFailed($tid, 'Encoding postprocessor: scp to chief-mirror failed');
+		$tracker->setTicketFailed($tid, 'Encoding postprocessor: scp to brisky failed');
 	}
 }
