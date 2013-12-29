27e8ce0a6338a58dbaaf600841906995ca3b8777
Peter Große <pegro@friiks.de>
2013-12-29 16:17:29 +0000

    tracker3.0: recording scheduler now supports moving to recorded on later calls
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@987 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- scripts/script-A-recording-scheduler.pl
+++ scripts/script-A-recording-scheduler.pl
@@ -27,10 +27,11 @@ my $target_state = 'recording';
 my $startpadding = 300;
 my $endpadding = 900;
 
-my $filter = {};
 # filter recording events
-$filter->{'Record.StartedBefore'} = strftime('%F %T',localtime(time + $startpadding));
-#$filter->{'Record.EndedBefore'} = localtime(time+$endpadding)->strftime('%F %T');
+my $start_filter = {};
+$start_filter->{'Record.StartedBefore'} = strftime('%F %T',localtime(time + $startpadding));
+my $end_filter = {};
+$end_filter->{'Record.EndedBefore'} = strftime('%F %T',localtime(time - $endpadding));
 
 #######################################
 
@@ -41,12 +42,12 @@ my $tracker = C3TT::Client->new($url, $token, $secret, 'record');
 my $tickets_left = 1;
 
 while($tickets_left) {
-    print "querying for ticket in state $target_state ...";
-    my $ticket = $tracker->assignNextUnassignedForState($target_type, $target_state, $filter);
+    print "querying for ticket with next state $target_state ...";
+    my $ticket = $tracker->assignNextUnassignedForState($target_type, $target_state, $start_filter);
 	print "\n";
 	if(!$ticket) {
 	    $tickets_left = 0;
-		print "no tickets currently $target_state. exiting...\n";
+		print "no tickets to be to moved to state $target_state. exiting...\n";
 		last;
 	}
 
@@ -60,3 +61,25 @@ while($tickets_left) {
     }
 	sleep 1;
 }
+
+# find assigned tickets in state recording
+
+print "querying for assigned ticket in state $target_state ...\n";
+my $tickets = $tracker->getAssignedForState($target_type, $target_state, $end_filter);
+
+if (!($tickets) || 0 == scalar(@$tickets)) {
+    print "no assigned tickets currently $target_state. exiting...\n";
+    exit 0;
+}
+
+print "found " . scalar(@$tickets) ." tickets\n";
+foreach (@$tickets) {
+    my $ticket = $_;
+    print "found ticket #" . $ticket->{id} . ". set done. ";
+
+    $tracker->setTicketDone($ticket->{id});
+    print "sleeping a second...\n";
+    sleep 1;
+}
+
+print "exit";
