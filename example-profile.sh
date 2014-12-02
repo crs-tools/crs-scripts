dadf60e52c6d8e9fce494d5ef487d70088a9770c
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2014-12-02 19:48:16 +0000

    reflect renamed scripts in configs
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@1099 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- example-profile.sh
+++ example-profile.sh
@@ -4,8 +4,14 @@
 export CRS_TRACKER=https://tracker.c3voc.de/rpc
 export CRS_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 export CRS_SECRET=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-export CRS_ROOM=H1
+
+# if you need to filter for a room
+#export CRS_ROOM=H1
+
+# if you need to filter for an encoding profile
+#export CRS_PROFILE=h264-hd
 
 # a place to store voc_ca.pem
 # needs to be a hashed dir (man c_rehash)
-#export HTTPS_CA_DIR=/opt/crs/software/configs/ssl/
+#export HTTPS_CA_DIR=/opt/crs/software/tools/tracker3.0/ssl/
+
