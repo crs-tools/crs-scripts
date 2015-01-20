595565c5fdf2491621ebdd24718653ca54b0db38
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2015-01-20 00:35:19 +0000

    support win32 shotcut options of fuse-ts
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@1122 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- lib/CRS/Fuse/TS.pm
+++ lib/CRS/Fuse/TS.pm
@@ -80,6 +80,8 @@ sub doFuseMount {
 	print "creating mount path \"$p\"\n" if defined($self->{debug});
 	qx ( mkdir -p "$p" );
 	my $fusecmd = " ".$self->{binpath}."/fuse-ts p=\"$prefix-\" c=\"$capdir\" st=\"$starttime\" numfiles=$files totalframes=$frames ";
+	$fusecmd .= " winpath=\"" . $self->{'Processing.Path.FuseWindowsPrefix'} . '" ' if (defined($self->{'Processing.Path.FuseWindowsPrefix'}));
+	$fusecmd .= " stripslashes=" . $self->{'Processing.Path.FuseWindowsStripSlashes'} . ' ' if (defined($self->{'Processing.Path.FuseWindowsStripSlashes'}));
 	$fusecmd .= " -oallow_other,use_ino \"$p\" ";
 
 	print "FUSE cmd: $fusecmd\n" if defined($self->{debug});
