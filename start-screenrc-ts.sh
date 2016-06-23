52fa0d6f71773f55c0f9c46ae77f8e5b2e9a931a
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2016-06-23 18:24:36 +0000

    no differences necessary anymore
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@1208 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- start-screenrc-ts.sh
+++ start-screenrc-ts.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-cd `dirname "$0"`
-
-if [ ! -f "tracker-profile.sh" ]; then
-	echo "tracker-profile.sh not found in `pwd`! " >&2
-	if [ -z "$CRS_TRACKER" -o -z "$CRS_TOKEN" -o -z "$CRS_SECRET" ]; then
-		echo 'Env vars CRS_TRACKER, CRS_TOKEN or CRS_SECRET are also empty.' >&2
-		echo 'Please create tracker-profile.sh or define env vars!' >&2
-		exit 1
-	fi
-fi
-
-screen -c ./screenrc-scripts
+echo 'This script is deprecated, please use start-screenrc-scripts.sh!' >&2
+exit 1
