2ef669dece982e20b8cf6e4a78c8b7b9a09493da
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2016-07-30 19:45:21 +0000

    implement complete atomic behaviour
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/cccongress/trunk@1247 8a601fa4-19c6-4598-94e3-4e3ea6015ade

--- scripts/script-D-encoding.pl
+++ scripts/script-D-encoding.pl
@@ -6,6 +6,9 @@ use boolean;
 
 my $tracker = C3TT::Client->new();
 my $ticket = $tracker->assignNextUnassignedForState('encoding', 'encoding');
+my $start = time;
+my $taskcount = 1;
+my $abortion = 0;
 
 if (!defined($ticket) || ref($ticket) eq 'boolean' || $ticket->{id} <= 0) {
 	print "currently no tickets for encoding\n";
@@ -17,30 +20,51 @@ if (!defined($ticket) || ref($ticket) eq 'boolean' || $ticket->{id} <= 0) {
 	my $jobxml = $tracker->getJobfile($tid);
 
 	my $ex = new CRS::Executor($jobxml);
+	$ex->setPreTaskFinishCallback(\&checkTicketStatus);
 
 	unless (defined($ex)) {
 		$tracker->setTicketFailed($tid, "Encoding script: instantiating job executor failed!");
 		exit;
 	}
 
-	my $time = time;
 	my $return = 0;
+	my $time = $start;
 
 	eval {
 		$return = $ex->execute();
 	};
 
-	$time = time - $time;
-
-	$log = join ("\n", $ex->getOutput());
+	$time = time - $start;
 
 	if ($return) {
-		$tracker->addLog($tid, $log);
+		# this is to verbose:
+		# $log = join ("\n", $ex->getOutput());
+		# $tracker->addLog($tid, $log);
+
 		$tracker->setTicketDone($tid, "Encoding tasks completed in $time seconds");
 		# indicate short sleep to wrapper script
 		exit(100);
 	} else {
+		$log = join ("\n", $ex->getErrors());
 		$tracker->addLog($tid, $log);
-		$tracker->setTicketFailed($tid, "Encoding tasks failed!");
+		# do not try to set failed if we aborted because of tracker issues:
+		$tracker->setTicketFailed($tid, "Encoding tasks failed!") unless $abortion;
 	}
+}
+
+sub checkTicketStatus() {
+	my $caller = shift;
+	my $ping = $tracker->ping($ticket->{id});
+
+	if ($ping eq 'OK') {
+		my $secs = time - $start;
+		$tracker->addLog($ticket->{id}, "task $taskcount completed after $secs seconds.");
+		$start = time;
+		$taskcount++;
+		return 1;
+	}
+	my $files = join (', ', $caller->getTemporaryFiles());
+	$tracker->addLog($ticket->{id}, "Aborting command execution after task $taskcount, leaving stale encoded files: $files");
+	$abortion = 1;
+	return 0;
 }
