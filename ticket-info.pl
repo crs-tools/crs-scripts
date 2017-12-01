668654601797e40d942eda5ff650f097cf213879
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2017-12-01 17:47:09 +0100

    fix ticket info helper script

--- ticket-info.pl
+++ ticket-info.pl
@@ -2,9 +2,13 @@
 
 BEGIN { push @INC, 'lib'; }
 
+use IO::Socket::SSL;
 require C3TT::Client;
-require Data::Dumper;
+use Data::Dumper;
 
+die "Please provide CRS settings via env variables (e.g. source tracker-profile.sh)" unless defined($ENV{CRS_TRACKER});
+my $tid=shift;
+die "Please give ticket ID (not Fahrplan ID) as first argument\n" unless defined ($tid);
 my $tracker = C3TT::Client->new();
 my $props = $tracker->getTicketProperties($tid);
 print "Properties of Ticket $tid:\n" . Dumper($props). "\n\n";
